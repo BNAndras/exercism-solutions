c65cd8e33306b13d8d5afd18732abd14476a4a9a
root <root@ip-10-1-2-190.eu-west-2.compute.internal>
2025-06-05 16:49:46 +0000

    [Sync Iteration] csharp/bird-watcher

---
+++ solutions/csharp/bird-watcher/BirdWatcher.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+
+class BirdCount(int[] birdsPerDay)
+{
+    public static int[] LastWeek() =>
+    [
+        0, 2, 5, 3, 7, 8, 4
+    ];
+
+    public int Today() =>
+        birdsPerDay[^1];
+
+    public void IncrementTodaysCount() =>
+        birdsPerDay[^1] += 1;
+
+    public bool HasDayWithoutBirds() =>
+        birdsPerDay.Any(count => count == 0);
+
+    public int CountForFirstDays(int numberOfDays) =>
+        birdsPerDay.Take(numberOfDays).Sum();
+
+    public int BusyDays() =>
+        birdsPerDay.Count(count => count >= 5);
+}
