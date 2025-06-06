37d9a56e5684b9e05f938ccc0518e7fa7c83c8b7
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:19:35 +0000

    [Sync Iteration] typescript/resistor-color

---
+++ solutions/typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,5 @@
+export const COLORS = ["black", "brown","red","orange","yellow","green","blue","violet","grey", "white"]
+
+export const colorCode = (code: string) : number => {
+  return COLORS.indexOf(code.toLowerCase());
+}
