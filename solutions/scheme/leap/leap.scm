fb0f1ca1cb77555d7059da89b1ac5cd1af20cb22
root <root@ip-10-1-1-59.eu-west-2.compute.internal>
2025-06-06 11:38:13 +0000

    [Sync Iteration] scheme/leap

--- solutions/scheme/leap/leap.scm
+++ solutions/scheme/leap/leap.scm
@@ -1,16 +1,10 @@
-(import (rnrs) (ice-9 match))
+(import (rnrs))
 
 (define (leap-year? year)
   ;; leap-year? : integer -> boolean
   ;; Is a Gregorian calendar year a leap year?
-  (let* ((divisible-by? (lambda (factor)
-                          (zero? (modulo year factor))))
-         (checks (list (divisible-by? 4)
-                       (divisible-by? 100)
-                       (divisible-by? 400))))
-    (match checks
-           ((_ _ #t) #t)
-           ((_ #t _) #f)
-           ((#t _ _) #t)
-           (_        #f))))
-
+  (let ((divisible-by? (lambda (factor)
+                            (zero? (modulo year factor)))))
+    (and (divisible-by? 4)
+         (or (not (divisible-by? 100))
+             (divisible-by? 400)))))
