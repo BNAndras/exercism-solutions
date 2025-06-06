e79a70517ba421b418626bc4b0f705914fbfe64d
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:50:49 +0000

    [Sync Iteration] scheme/grains

--- solutions/scheme/grains/grains.scm
+++ solutions/scheme/grains/grains.scm
@@ -3,19 +3,10 @@
 (define (square n)
   ;; square : integer -> integer
   ;; Return the number of grains on the n-th chessboard square
-  (define (square-rec n acc)
-    (if (eq? n 1)
-      acc
-      (square-rec (- n 1) (* 2 acc))))
   (assert (<= 1 n 64))
-  (square-rec n 1))
-
+  (expt 2 (- n 1)))
 
 (define (total)
   ;; total : -> integer
   ;; Return the total number of grains on a filled chessboard
-  (define (total-rec n acc)
-    (if (eq? n 1)
-        acc
-        (total-rec (- n 1) (+ acc (square n)))))
-    (total-rec 64 1))
+  (- (expt 2 64) 1))
