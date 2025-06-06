ec3525b31098f88ec8f5f4215ee13328574161e4
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:50:55 +0000

    [Sync Iteration] typescript/resistor-color-duo

--- solutions/typescript/resistor-color-duo/resistor-color-duo.ts
+++ solutions/typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,8 @@
 export function decodedValue([band1, band2]: Array<string>): number {
   if (!(COLORS.includes(band1) && COLORS.includes(band2))) {
-    throw new Error(`Invalid bands passed to ${decodedValue.name}(${band1},${band2}).`);
+    throw new Error(`Invalid bands passed to ${decodedValue.name}(${band1}, ${band2}).`);
   }
-  Array.from(arguments).join(",")
+  
   const value1 = COLORS.indexOf(band1);
   const value2 = COLORS.indexOf(band2);
   return Number(`${value1}${value2}`)
