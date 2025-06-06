042e459d2efe07dbd8b75ddabd005286a5a1be03
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:19:15 +0000

    [Sync Iteration] common-lisp/space-age

--- solutions/common-lisp/space-age/space-age.lisp
+++ solutions/common-lisp/space-age/space-age.lisp
@@ -3,11 +3,11 @@
 
 (in-package :space-age)
 
-(defconstant seconds-in-orbital
+(defparameter +seconds-in-orbital+
   31557600
   "number of Earth seconds in an one full Earth orbital period")
 
-(defconstant orbital-ratios
+(defparameter +orbital-ratios+
   '(("Mercury"  . 0.2408467)
     ("Venus"    . 0.61519726)
     ("Earth"    . 1)
@@ -18,13 +18,13 @@
     ("Neptune"  . 164.79132))
     "Orbital periods relative to Earth's orbital period")
 
-(dolist (pair orbital-ratios)
+(dolist (pair +orbital-ratios+)
   (destructuring-bind (planet . ratio) pair
     (let ((label (intern (concatenate 'string "ON-" (string-upcase planet)))))
       (progn
         (setf (symbol-function label)
               (lambda (earth-seconds)
-                (/ earth-seconds (* seconds-in-orbital ratio))))
+                (/ earth-seconds (* +seconds-in-orbital+ ratio))))
         (setf (documentation label 'function)
               (format nil "Given seconds on Earth, the local age on ~a." planet))
         (export label)))))
