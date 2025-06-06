7034f26cfc1416cf4bbf5f7eb935a90fcc3a514b
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:19:31 +0000

    [Sync Iteration] typescript/two-fer

---
+++ solutions/typescript/two-fer/two-fer.ts
@@ -0,0 +1,7 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(person: string = "you"): string {
+  return `One for ${person}, one for me.`;
+}
