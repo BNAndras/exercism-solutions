a4d89b70d23b1124924e6317e34d08c97297dda4
Ubuntu <ubuntu@ip-10-1-2-205.eu-west-2.compute.internal>
2025-06-06 11:23:55 +0000

    [Sync Iteration] typescript/resistor-color-trio

--- solutions/typescript/resistor-color-trio/resistor-color-trio.ts
+++ solutions/typescript/resistor-color-trio/resistor-color-trio.ts
@@ -9,9 +9,11 @@ export const COLORS = [
   "violet",
   "grey",
   "white",
-];
+] as const
 
-export function decodedResistorValue([band1, band2, band3]: Array<string>) : string {
+type Color = typeof COLORS[number];
+
+export function decodedResistorValue([band1, band2, band3]: Array<Color>) : string {
   const value1 = COLORS.indexOf(band1);
   const value2 = COLORS.indexOf(band2);
   const value3 = COLORS.indexOf(band3);
@@ -19,7 +21,7 @@ export function decodedResistorValue([band1, band2, band3]: Array<string>) : str
   let val =  Number(`${value1}${value2}${"0".repeat(value3)}`);
   let unit = "ohms";
   if (val >= 1000) {
-    val = val / 1000;
+    val /= 1000;
     unit = "kiloohms";
   }
   return `${val} ${unit}`;
