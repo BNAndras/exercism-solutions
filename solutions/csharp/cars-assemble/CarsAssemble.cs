42b99e717ca6c3ebf67f02de64de831b1aab7008
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:18:26 +0000

    [Sync Iteration] csharp/cars-assemble

---
+++ solutions/csharp/cars-assemble/CarsAssemble.cs
@@ -0,0 +1,21 @@
+static class AssemblyLine
+{
+    private static int BaseProductionRatePerHour => 221;
+    
+    public static double SuccessRate(int speed) =>
+        speed switch
+        {
+            <= 0 => 0.0,
+            <= 4 => 1.0,
+            <= 8 => 0.9,
+            9 => 0.8,
+            10 => 0.77,
+            _ => 0.0
+        };
+
+    public static double ProductionRatePerHour(int speed) =>
+        BaseProductionRatePerHour * speed * SuccessRate(speed);
+    
+    public static int WorkingItemsPerMinute(int speed) =>
+        (int) (ProductionRatePerHour(speed) / 60);
+}
