9626af3aa6fbe8a09892f0b53ea8d968b4dd5003
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:19:23 +0000

    [Sync Iteration] scheme/grains

---
+++ solutions/scheme/grains/grains.scm
@@ -0,0 +1,12 @@
+(import (rnrs))
+
+(define (square n)
+  ;; square : integer -> integer
+  ;; Return the number of grains on the n-th chessboard square
+  (assert (<= 1 n 64))
+  (expt 2 (- n 1)))
+
+(define (total)
+  ;; total : -> integer
+  ;; Return the total number of grains on a filled chessboard
+  (- (expt 2 64) 1))
