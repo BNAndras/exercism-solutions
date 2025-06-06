625cce995b8fe4ac5c6794eefc6c873edc25eef4
root <root@ip-10-1-0-66.eu-west-2.compute.internal>
2025-06-06 11:39:58 +0000

    [Sync Iteration] clojure/reverse-string

--- solutions/clojure/reverse-string/src/reverse_string.clj
+++ solutions/clojure/reverse-string/src/reverse_string.clj
@@ -2,7 +2,6 @@
 
 (require '[clojure.string :as str])
 
-(defn reverse-string
-  [s]
-   "Reverses string, treating Unicode surogate pairs as a single character"
-  (str/reverse s))
+(defn reverse-string [s] ;; arguments vector...
+  (str/reverse s)
+  )
