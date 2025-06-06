b86f0a2567173753d9734c301aae4ef38eb4882e
root <root@ip-10-1-1-59.eu-west-2.compute.internal>
2025-06-06 11:38:09 +0000

    [Sync Iteration] common-lisp/space-age

--- solutions/common-lisp/space-age/space-age.lisp
+++ solutions/common-lisp/space-age/space-age.lisp
@@ -5,28 +5,32 @@
 
 (defconstant seconds-in-orbital
   31557600
-  "Bumber of Earth seconds in a single orbital period")
+  "number of Earth seconds in one full Earth orbital period")
+
+(defconstant ratios
+  '(("Mercury"  0.2408467)
+    ("Venus"    0.61519726)
+    ("Earth"    1)
+    ("Mars"     1.8808158)
+    ("Jupiter"  11.862615)
+    ("Saturn"   29.447498)
+    ("Uranus"   84.016846)
+    ("Neptune"  164.79132)))
 
 (defparameter orbital-ratios
-  (make-hash-table)
-  "Lookup table of orbital ratios relative to Earth")
-
-(setf (gethash "Mercury" orbital-ratios) 0.2408467)
-(setf (gethash "Venus" orbital-ratios)   0.61519726)
-(setf (gethash "Earth" orbital-ratios)   1)
-(setf (gethash "Mars" orbital-ratios)    1.8808158)
-(setf (gethash "Jupiter" orbital-ratios) 11.862615)
-(setf (gethash "Saturn" orbital-ratios)  29.447498)
-(setf (gethash "Uranus" orbital-ratios)  84.016846)
-(setf (gethash "Neptune" orbital-ratios) 164.79132)
+  (let ((hash-table (make-hash-table)))
+    (dolist (pair ratios)
+      (destructuring-bind (planet ratio) pair
+        (setf (gethash planet hash-table) ratio)))
+    hash-table))
 
 (maphash (lambda (planet ratio)
-                 (let ((label (intern (concatenate 'string "ON-" (string-upcase planet)))))
-                   (progn
-                     (setf (symbol-function label)
-                           (lambda (earth-seconds)
-                             (/ earth-seconds (* seconds-in-orbital ratio))))
-                     (setf (documentation label 'function)
-                           (format nil "Given seconds on Earth, the local age on ~a." planet))
-                    (export label))))
+           (let ((label (intern (concatenate 'string "ON-" (string-upcase planet)))))
+             (progn
+               (setf (symbol-function label)
+                     (lambda (earth-seconds)
+                       (/ earth-seconds (* seconds-in-orbital ratio))))
+               (setf (documentation label 'function)
+                     (format nil "Given seconds on Earth, the local age on ~a." planet))
+               (export label))))
          orbital-ratios)
