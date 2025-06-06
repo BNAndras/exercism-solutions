0f8fe57b6d4bf98690ef922f92b8bb43d211c4bd
root <root@ip-10-1-0-66.eu-west-2.compute.internal>
2025-06-06 11:40:10 +0000

    [Sync Iteration] typescript/rna-transcription

--- solutions/typescript/rna-transcription/rna-transcription.ts
+++ solutions/typescript/rna-transcription/rna-transcription.ts
@@ -1,26 +1,20 @@
-enum nucleotide {
-  adenine = "A",
-  cytosine = "C",
-  guanine = "G",
-  thymine = "T",
-  uracil = "U",
-}
-
 const ERR_INVALID_INPUT = "Invalid input DNA.";
 const REGEX_TEST_FOR_INVALID_NUC = new RegExp("[^CGAT]","g")
-const REGEX_FIND_VALID_NUC = new RegExp("[CGAT]","g")
 
-const complements = new Map([
-  [nucleotide.guanine, nucleotide.cytosine],
-  [nucleotide.cytosine, nucleotide.guanine],
-  [nucleotide.thymine, nucleotide.adenine],
-  [nucleotide.adenine, nucleotide.uracil],
-]);
+const complements: { [key: string]: string } = {
+  "G": "C",
+  "C": "G",
+  "T": "A",
+  "A": "U",
+}
 
 export function toRna(dna: string): string {
   if (dna.match(REGEX_TEST_FOR_INVALID_NUC)) {
     throw new Error(ERR_INVALID_INPUT);
   }
 
-  return dna.replace(REGEX_FIND_VALID_NUC, (c) => complements.get(c as nucleotide) as string)
+  return [...dna].reduce((output, letter) => {
+    output += complements[letter];
+    return output
+  }, '')
 }
