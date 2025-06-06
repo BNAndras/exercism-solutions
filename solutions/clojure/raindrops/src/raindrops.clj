33c19d84e1162393d7078d5f38ce2a341372bdd4
root <root@ip-10-1-0-66.eu-west-2.compute.internal>
2025-06-06 11:39:59 +0000

    [Sync Iteration] clojure/raindrops

--- solutions/clojure/raindrops/src/raindrops.clj
+++ solutions/clojure/raindrops/src/raindrops.clj
@@ -1,23 +1,20 @@
 (ns raindrops)
 
 (def rules
-  "Rules consisting of a factor and a representative sound"
+  "Rules consisting of a factor and a representative sound string"
   `[[3 "Pling"] [5 "Plang"] [7 "Plong"]])
 
 (defn- get-sound
   [n [factor sound]]
-  "Return sound if n is divisible by factor"
-  (when (zero? (mod n factor))
-     sound)
-)
+  "Return a sound if n is divisible by factor or an empty string otherwise"
+  (if (zero? (mod n factor))
+     sound
+      ""))
 
 (defn convert
   [n]
-  "Return string of sounds representing a number's factors or the digits if not divisible by 3, 5, or 7"
-  (let [sounds (->> rules
-                    (reduce #(conj %1 (get-sound n %2)) `[])
-                    (filter (complement nil?)))]
-    (if (empty? sounds)
-      (str n)
-      (apply str sounds))))
-  
+  "Convert a number into a string based on its factors, each representing a sound. If no factors are found, the number's digits are returned as the string"
+  (let [sounds (reduce #(str %1 (get-sound n %2)) "" rules)]
+    (if-not (empty? sounds)
+      sounds
+      (str n))))
