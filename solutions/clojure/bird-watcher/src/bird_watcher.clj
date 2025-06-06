087e8178c477262b17babfb01768c585007a8865
root <root@ip-10-1-1-59.eu-west-2.compute.internal>
2025-06-06 11:38:12 +0000

    [Sync Iteration] clojure/bird-watcher

--- solutions/clojure/bird-watcher/src/bird_watcher.clj
+++ solutions/clojure/bird-watcher/src/bird_watcher.clj
@@ -14,16 +14,20 @@
 
 (defn day-without-birds? [birds]
   "Check if there's a day with no birds logged"
-  (not (every? pos? birds)))
+  (true? (some zero? birds)))
 
 (defn n-days-count [birds n]
   "Return total birds seen in the first n days from the beginning of the week"
-  (reduce + (take n birds)))
+  (reduce + (subvec birds 0 n)))
+
+(defn busy-day? [day-count]
+  "Check if a day was a busy one for bird-watching"
+  (> day-count 4))
 
 (defn busy-days [birds]
   "Count the number of busy days"
-  (count (filter #(> % 4)  birds)))
+  (count (filter busy-day? birds)))
 
 (defn odd-week? [birds]
-  "Check if the week has alternating 1 and 0 counts"
-  (= (take (count birds) (cycle [1 0]))  birds))
+  "Check if the week has alternating 0 and 1 counts"
+  (= `[1 0 1 0 1 0 1] birds))
