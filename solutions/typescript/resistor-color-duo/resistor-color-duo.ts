5f1a5173db1c3855846b033d662719c30148cf07
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:50:55 +0000

    [Sync Iteration] typescript/resistor-color-duo

--- solutions/typescript/resistor-color-duo/resistor-color-duo.ts
+++ solutions/typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,18 +1,22 @@
-enum ResistorValue {
-    black,
-    brown,
-    red,
-    orange,
-    yellow,
-    green,
-    blue,
-    violet,
-    grey,
-    white,
+export function decodedValue([band1, band2]: Array<string>): number {
+  if (!(COLORS.includes(band1) && COLORS.includes(band2))) {
+    throw new Error(`Invalid bands passed to ${decodedValue.name}(${band1},${band2}).`);
+  }
+  Array.from(arguments).join(",")
+  const value1 = COLORS.indexOf(band1);
+  const value2 = COLORS.indexOf(band2);
+  return Number(`${value1}${value2}`)
 }
 
-type BandColor = keyof typeof ResistorValue;
-
-export function decodedValue([first, second]: BandColor[]): number {  
-  return ResistorValue[first] * 10 + ResistorValue[second];  
-}
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
