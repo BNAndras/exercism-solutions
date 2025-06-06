331c26dbdc2075ad93216ec2e6c158a2eb6e7dfb
Ubuntu <ubuntu@ip-10-1-2-205.eu-west-2.compute.internal>
2025-06-06 11:23:51 +0000

    [Sync Iteration] typescript/two-fer

--- solutions/typescript/two-fer/two-fer.ts
+++ solutions/typescript/two-fer/two-fer.ts
@@ -1,5 +1,7 @@
-type Name = boolean;
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
 
 export function twoFer(person: string = "you"): string {
-  return `One for ${person}, one for me.`; 
+  return `One for ${person}, one for me.`;
 }
